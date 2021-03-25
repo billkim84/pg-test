e86367e7eb85be7835944563644b4fa29d98e5a9
Bill Kim <bill.jh.kim@gmail.com>
2021-03-25 17:38:48 +0900

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,46 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {useState} from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const [email, setEmail] = useState('test@test.com');
+  const [section, setSection] = useState('login');
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+  async function startApplication() {
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+    const res = await fetch('http://ec2-13-125-166-148.ap-northeast-2.compute.amazonaws.com:3000/application', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify( {
+        userId: email
+      })
+    })
+    const data = await res.json();
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+    console.log(data);
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+    window.location = `https://7nwady7uv3k.typeform.com/to/Z4iI6mjC#application_id=${data.applicationId}&user_id=${email}`;
+  }
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+  if(section === 'login') {
+    return <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100vh'}}>
+      <div style={{ width: 200, display: 'flex', flexDirection: 'column'}}>
+        email:
+        <input value={email} onChange={(e) => setEmail(e.target.value)} style={{marginBottom: 20}}/>
+        password:
+        <input type="password" style={{marginBottom: 20}}/>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <button onClick={() => setSection('loggedin')}>Login</button>
+      </div>
     </div>
-  )
+  } else {
+    return <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100vh'}}>
+      <div>Welcome {email}</div>
+      <button onClick={startApplication}>Start application </button>
+    </div>
+  }
+
 }
